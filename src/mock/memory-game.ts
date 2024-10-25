779b59f5dd4407e771d5abbdd7801681df2dd706
Shole Alirezaei <alirezaei.shole@gmail.com>
2024-10-25 15:56:58 +0330

    create a constant for cards

---
+++ src/mock/memory-game.ts
@@ -0,0 +1,109 @@
+import {
+  Android,
+  Apple,
+  Music,
+  Pet,
+  Reserve,
+  Sun1,
+  Tree,
+  Windows,
+} from 'iconsax-react'
+
+export const cardItems = [
+  {
+    id: 1001,
+    img: Apple,
+    color: '#2ccce4',
+    stat: '',
+  },
+  {
+    id: 1001,
+    img: Apple,
+    color: '#2ccce4',
+    stat: '',
+  },
+  {
+    id: 1002,
+    img: Android,
+    color: '#ff8a65',
+    stat: '',
+  },
+  {
+    id: 1002,
+    img: Android,
+    color: '#ff8a65',
+    stat: '',
+  },
+  {
+    id: 1003,
+    img: Pet,
+    color: '#ba68c8',
+    stat: '',
+  },
+  {
+    id: 1003,
+    img: Pet,
+    color: '#ba68c8',
+    stat: '',
+  },
+  {
+    id: 1004,
+    img: Windows,
+    color: '#dce775',
+    stat: '',
+  },
+  {
+    id: 1004,
+    img: Windows,
+    color: '#dce775',
+    stat: '',
+  },
+  {
+    id: 1005,
+    img: Tree,
+    color: '#37d67a',
+    stat: '',
+  },
+  {
+    id: 1005,
+    img: Tree,
+    color: '#37d67a',
+    stat: '',
+  },
+  {
+    id: 1006,
+    img: Sun1,
+    color: '#f47373',
+    stat: '',
+  },
+  {
+    id: 1006,
+    img: Sun1,
+    color: '#f47373',
+    stat: '',
+  },
+  {
+    id: 1007,
+    img: Reserve,
+    color: '#697689',
+    stat: '',
+  },
+  {
+    id: 1007,
+    img: Reserve,
+    color: '#697689',
+    stat: '',
+  },
+  {
+    id: 1008,
+    img: Music,
+    color: '#facc15',
+    stat: '',
+  },
+  {
+    id: 1008,
+    img: Music,
+    color: '#facc15',
+    stat: '',
+  },
+]
